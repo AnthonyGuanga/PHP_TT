728e07f51551345f9887862952361ff30f92c3fc
AnthonyGuanga <ninfrad23@gmail.com>
2025-01-28 13:56:40 +0100

    add nuevas implementacion

--- sql_book/aplicacion.php
+++ sql_book/aplicacion.php
@@ -1,30 +1,31 @@
 <?php
 require_once "DBConnection.php";
 require_once "customer.php";
-
+require_once "book.php";
 $db = new DBConnection();
 $conexion = $db->getConnection();
-if ($conexion) {
-    try{
-        $sql = "SELECT * FROM book";
-        $stmt = $conexion->prepare($sql);
-        $stmt->execute();
-        $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        var_dump($resultados);
+// if ($conexion) {
+//     try{
+//         $sql = "SELECT * FROM book";
+//         $stmt = $conexion->prepare($sql);
+//         $stmt->execute();
+//         $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//         var_dump($resultados);
         
-        foreach($resultados as $book){
-            echo "Titulo: ".$book['title']."<br>";
-            echo "Autor: ".$book['author']."<br>";
-            echo "Precio: ".$book['price']."<br>";
-            echo "Stock: ".$book['stock']."<br>";
-            echo "<hr>";
-        }
-    }catch(PDOException $e){
-        echo "Error en la consulta";
-    }
-} else {
-    echo "Error en la conexión";
-}
-// adriancadillo
-// proyectosistemas
+//         foreach($resultados as $book){
+//             echo "Titulo: ".$book['title']."<br>";
+//             echo "Autor: ".$book['author']."<br>";
+//             echo "Precio: ".$book['price']."<br>";
+//             echo "Stock: ".$book['stock']."<br>";
+//             echo "<hr>";
+//         }
+//     }catch(PDOException $e){
+//         echo "Error en la consulta";
+//     }
+// } else {
+//     echo "Error en la conexión";
+// }
+$book = new Book();
+$crear = $book->inserterLibrros();
+
 
---
+++ sql_book/book.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "utils.php";
+spl_autoload_register('classAutoLoader');
+class Book{
+private $isbn;
+private $title;
+private $author;
+private $stock;
+private $price;
+
+public function __construct($isbn = null, $title= null, $author= null,$stock= null,$price= null){
+    $this->isbn=$isbn;
+    $this->title=$title;
+    $this->author=$author;
+    $this->stock=$stock;
+    $this->price=$price;
+}
+
+public function inserterLibrros(){
+    $conObj = new DBconnection();
+    $con=$conObj->getConnect();
+    $stmt = $con->prepare("INSERT INTO book (isbn, title, author, stock, price) VALUES (:isbn, :title, :author, :stock, :price)");
+    try{
+        $stmt->execute([
+            'isbn' => $this->isbn,
+            'title' => $this->title,
+            'author' => $this->author,
+            'stock' => $this->stock,
+            'price' => $this->price
+        ]);
+        echo "Libro insertado";
+    }catch(PDOException $e){
+        echo "Error al insertar el libro";
+    }
+}
+
+    
+}
+
+
