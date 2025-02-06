5ab2ef17587ab35e40a410bd70aede3a4460270c
AnthonyGuanga <ninfrad23@gmail.com>
2025-02-06 11:12:38 +0100

    add nuevos cambios

--- sql_book/registre.php
+++ sql_book/registre.php
@@ -1,46 +1,18 @@
-<?php
-require_once "DBConnection.php";
-
-echo <<< eot
-<h1>Registro de usuarios</h1>
-<form action="#" method="post">
-    <label>Nombre:
-        <input type="text" name="nombre" required />
-    </label>
-    <br/>
-    <label>Apellido:
-        <input type="text" name="apellido" required />
-    </label>
-    <br/>
-    <label>Email:
-        <input type="email" name="email" required />
-    </label>
-    <br/>
-    <label>Contraseña:
-        <input type="password" name="contrasenia" required />
-    </label>
-    <br/>
-    <input type="submit" name="submit" value="Confirm" />
-</form>
-eot;
-
-if (isset($_POST["submit"])) {
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $email = $_POST['email'];
-    $contrasenia = $_POST['contrasenia'];
-    $db = new DBConnection();
-    $connexion = $db->getConnection();
-    $db->insertData("customer", [
-        "id" => 1,
-        "firstname" => $nombre,
-        "surname" => $apellido,
-        "email" => $email,
-        "type" => "premium",
-        "password" => $contrasenia
-    ]);
-    var_dump($connexion);
-    var_dump($db);
-    echo "Usuario registrado";
-    //header("Refresh:5, url=login.php");
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome to the registry</h1>
+    <a href="login.php"><button>Go back to login</button></a>
+    <?php
+    
+    print("<form>
+    
+    </form>");
+    ?>
+</body>
+</html>
