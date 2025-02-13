351ca6f4cd904b89962423b8de99017862a9142e
AnthonyGuanga <ninfrad23@gmail.com>
2025-02-13 10:24:25 +0100

    add nuevas implementaciones

---
+++ sql_book/register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome to the registry</h1>
+    <a href="login.php"><button>Go back to login</button></a>
+    <?php
+    
+    print("<form action=\"./validateRegister.php\ method=\"POST\">
+    <br/>
+    <label>
+        Firsname :
+        <input type=\"text\" name=\"firsname\" placeholder=\"Your name\"/>
+    </label>
+    <br/>
+    <label>
+        Surname :
+        <input type=\"text\" name=\"surname\" placeholder=\"Your surname\"/>
+    </label>
+    <br/>
+<label>
+        Email :
+        <input type=\"text\" name=\"email\" placeholder=\"example@email.com\"/>
+    </label>
+    <br/>
+    <label>
+        Password :
+        <input type=\"text\" name=\"password\" placeholder=\"your password\"/>
+    </label>
+     <br/>
+    <label>
+        <input type=\"radio\" name=\"type\" value=\"basic\" checked> Basic </input>
+        <input type=\"radio\" name=\"type\" value=\"premiun\" > Premiun </input>
+    </label>
+    </br>
+    <input type=\"submit\" name=\"register\" value=\"confirm\"/>
+    </form>");
+    ?>
+</body>
+</html>
---
+++ sql_book/validateRegister.php
@@ -0,0 +1,16 @@
+<?php
+include_once "./clases/customer.php";
+require_once "./utils.php";
+
+if(!isset($_POST["firsname"])|!isset($_POST["surname"])|!isset($_POST["email"])|!isset($_POST["password"])|!isset($_POST["type"])){
+    header("location:./register.php");
+}
+$customer= new customer($_POST["email"],$_POST["password"],$_POST["type"],$_POST["firsname"],$_POST["surname"]);
+
+if(addCustomer($customer)){
+    echo "<h2>User created correctly, you will be redirected in 5 seconds</h2>";
+    header("Refresh: 5; URL=./index.php");
+} else {    
+    echo "<h2>Error creating user. Please try again.</h2>";
+    header("Refresh: 5; URL=./register.php");
+}
